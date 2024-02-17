3526a8bbca53d8ba9bc9faef35f3cc534e59f5ca
Yaseen Younus <yay200@gmail.com>
2024-02-17 13:36:25 -0600

    Adding code to mask secret phrase

--- hangman.go
+++ hangman.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
+	secretPhrase := "Thanks for playing Go Hangman"
+
 	fmt.Println("Welcome to Go Hangman!")
+	fmt.Println(secretPhrase, utf8.RuneCountInString(secretPhrase))
+
+	for _, char := range secretPhrase {
+		if char == 32 {
+			fmt.Print(" ")
+		} else {
+			fmt.Print("_")
+		}
+	}
 }
